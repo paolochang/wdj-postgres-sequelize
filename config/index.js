d3f4f25346a396be5124ae32b57b6b319462323e
Paolo Chang <chanhwan.chang@gmail.com>
2022-11-25 22:34:42 -0500

    01 Setup Postgres

--- config/index.js
+++ config/index.js
@@ -1,12 +1,13 @@
-const bunyan = require('bunyan');
+const bunyan = require("bunyan");
 // Load package.json
-const pjs = require('../package.json');
+const pjs = require("../package.json");
 
 // Get some meta info from the package.json
 const { name, version } = pjs;
 
 // Set up a logger
-const getLogger = (serviceName, serviceVersion, level) => bunyan.createLogger({ name: `${serviceName}:${serviceVersion}`, level });
+const getLogger = (serviceName, serviceVersion, level) =>
+  bunyan.createLogger({ name: `${serviceName}:${serviceVersion}`, level });
 
 // Configuration options for different environments
 module.exports = {
@@ -14,18 +15,30 @@ module.exports = {
     name,
     version,
     serviceTimeout: 30,
-    log: () => getLogger(name, version, 'debug'),
+    postgres: {
+      options: {
+        host: "localhost",
+        port: 5432,
+        database: "wdj_tutorial",
+        dialect: "postgres",
+        username: "postgres",
+        password: "password",
+        logging: (msg) => getLogger(name, version, "debug").info(msg),
+      },
+      client: null,
+    },
+    log: () => getLogger(name, version, "debug"),
   },
   production: {
     name,
     version,
     serviceTimeout: 30,
-    log: () => getLogger(name, version, 'info'),
+    log: () => getLogger(name, version, "info"),
   },
   test: {
     name,
     version,
     serviceTimeout: 30,
-    log: () => getLogger(name, version, 'fatal'),
+    log: () => getLogger(name, version, "fatal"),
   },
 };
